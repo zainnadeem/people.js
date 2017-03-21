c8e5d5831e3bfee47edb5e3ae666869bd6ebd32f
zainnadeem <zn.nadeem@gmail.com>
2017-03-21 17:43:31 -0400

    updated landing page

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) =>  {
-   res.send('Home page for Spotify Challenge')
+   res.send('Spotify Fellowship Challenge Zain Nadeem')
 })
 
 app.get("/people", (req, res, next) => {
